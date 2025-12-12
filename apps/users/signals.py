3d8e5cd87077dd2ade8b8aca2bfd7b55e1b2c601
Wasik Ahmed Apon <aponwasikahmed@gmail.com>
2025-12-12 06:58:13 +0600

    Add auto role assign after google login/signup and update profile info

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -3,6 +3,9 @@ from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 
+# Import the signal from allauth
+from allauth.account.signals import user_signed_up
+
 User = get_user_model()
 
 
@@ -11,3 +14,42 @@ def assign_group_to_superuser(sender, instance, created, **kwargs):
     if created and instance.is_superuser:
         admin_group, _ = Group.objects.get_or_create(name='Admin')
         instance.groups.add(admin_group)
+
+
+@receiver(user_signed_up)
+def populate_profile(request, user, **kwargs):
+    """
+    Used for Social Auth (Google) signals.
+    1. Assign 'Student' group to new social users.
+    2. Extract 'full_name' from Google profile.
+    """
+    # -----------------------------------------------------------
+    # 1. Assign Role (Group)
+    # -----------------------------------------------------------
+    # We use get_or_create to be safe, just like in your serializers
+    student_group, _ = Group.objects.get_or_create(name='Student')
+    user.groups.add(student_group)
+
+    # -----------------------------------------------------------
+    # 2. Extract Name from Google
+    # -----------------------------------------------------------
+    # The signal passes a 'sociallogin' object in kwargs
+    sociallogin = kwargs.get('sociallogin')
+    
+    if sociallogin and sociallogin.account.provider == 'google':
+        # Google returns useful data in extra_data
+        data = sociallogin.account.extra_data
+        
+        # Try to get the 'name' field (e.g., "Wasik Ahmed")
+        google_name = data.get('name')
+        
+        # If 'name' is missing, try combining given_name + family_name
+        if not google_name:
+            given_name = data.get('given_name', '')
+            family_name = data.get('family_name', '')
+            google_name = f"{given_name} {family_name}".strip()
+
+        # Save it to your custom User model
+        if google_name:
+            user.full_name = google_name
+            user.save()
